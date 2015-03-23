d57214ec691c039eff112ff02d97e64f51f38d99
novicaa <novicamiketic@gmail.com>
2015-03-23 14:20:09 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+$adresa="1001 Crosspointe Drive Suite 2 Naples, FL 34110 "; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+$telephone="2395662422";// Broj koji se poziva na dugme CALL US
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
