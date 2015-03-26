7007c239d2da29327ab58b92b9ba729fbc013d20
novicaa <novicamiketic@gmail.com>
2015-03-26 13:49:55 +0100

    promjene footer

--- first/index.php
+++ first/index.php
@@ -115,7 +115,8 @@
 		    <h1><img src="img/logo.png" alt="LOGO"></h1>
 		</div>
 		<div data-role="footer" data-theme="<?php echo $theme ?>" data-position="fixed">
-		    <h1>Mobile Site<a href="http://www.brettblacher.com/" rel="external">|Classic Site</a></h1>
+            <h1>Powered by <a href="http://www.mobilegrows.com" rel="external">MobileGrows</a> | <a href="http://www.thedentistrynaples.com/" rel="external">Desktop Site</a></h1>
+
 		</div>
 		<!-- page 1 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////-->
 		<div data-role="page" id="page1" data-theme="<?php echo $theme ?>">
